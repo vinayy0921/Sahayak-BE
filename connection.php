117bfff24f0f508e02029e43ec0c368526881ce0
vinaysahani <vinaysahani248@gmail.com>
2025-11-30 13:54:34 +0530

    Backend Initialized

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+// config/db_connect.php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sahayak"; 
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die(json_encode(["status" => "error", "message" => "Connection failed: " . $conn->connect_error]));
+}
+?>
