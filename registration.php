bd8e88399bb84b87f617528126317a210df7f78a
Harmit Patel <harmitpatel0312@gmail.com>
2025-12-02 19:10:34 +0530

    added

--- registration.php
+++ registration.php
@@ -5,7 +5,8 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-include 'connection.php';
+include __DIR__ . '/../config/db_connect.php';
+
 
 $data = json_decode(file_get_contents("php://input"));
 
